a5a51eedbdc1bc405f783071e2df06fc118c08fd
Charley <charleyann.robertson@gmail.com>
2025-04-18 20:35:25 +0100

    Dynamic content based on hard coded content

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Book } from '../book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
+
+  books: Book[] = [
+    { bookid: 1, title: "Book one", author: "Author one" },
+    { bookid: 2, title: "Book two", author: "Author two" },
+    { bookid: 3, title: "Book three", author: "Author three" }
+  ]
 
 }
